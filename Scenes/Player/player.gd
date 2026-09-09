e791c3d29512c04db8cdadeacd2e75d3a501fc30
Jerome <jerome.riffier@hotmail.fr>
2026-09-09 17:31:54 +0200

    Finish managing and testing is_playable cards

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -14,15 +14,24 @@ var is_alive:bool = false
 
 signal has_played
 
-func _ready() -> void:
-	## For debug only, card should be received from GameManager
-	#for i in range(3):
-		#var card:Card = preload("uid://c7em3t5lv5vr0").instantiate()
-		#add_child(card)
-		#card.global_position = Vector2.ZERO
-		#draw_card(card)
-		#await get_tree().create_timer(0.5).timeout
-	pass
+#func _ready() -> void:
+	### For debug only, card should be received from GameManager
+	#await get_tree().create_timer(0.5).timeout
+	#var card:Card = preload("uid://c7em3t5lv5vr0").instantiate()
+	#add_child(card)
+	#card.set_infos(Constant.ROI)
+	#card.global_position = Vector2.ZERO
+	#draw_card(card)
+	#await get_tree().create_timer(0.5).timeout
+	#
+	#var card_2:Card = preload("uid://c7em3t5lv5vr0").instantiate()
+	#add_child(card_2)
+	#card_2.set_infos(Constant.COMTESSE)
+	#card_2.global_position = Vector2.ZERO
+	#draw_card(card_2)
+	#await get_tree().create_timer(0.5).timeout
+	#is_playing = true
+	
 
 func draw_card(card:Card) -> void:
 	hand.add_card_to_hand(card)
@@ -62,9 +71,12 @@ func finish_drag() -> void:
 	card_being_dragged = null
 
 func is_card_playable(card:Card) -> bool:
-	if [Constant.PRINCE, Constant.ROI].has(card.infos) and hand.cards.find_custom(func (card:Card) -> bool: return card.infos == Constant.COMTESSE):
+	var is_roi_or_prince:bool = [Constant.PRINCE, Constant.ROI].has(card.infos)
+	var hand_contain_comtesse:bool = hand.cards.any(func (element:Card) -> bool: return element.infos == Constant.COMTESSE)
+	if is_roi_or_prince and hand_contain_comtesse:
 		return false
 	return true
+
 #region input
 ### 
 ## Check if the mouse position collide with a card
