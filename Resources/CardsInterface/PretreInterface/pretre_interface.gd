c4e7f9e283675351d0eb6740ac31e69a4450e984
Jerome <jerome.riffier@hotmail.fr>
2026-09-10 13:46:14 +0200

    Setup PretreInterface

--- Resources/CardsInterface/PretreInterface/pretre_interface.gd
+++ Resources/CardsInterface/PretreInterface/pretre_interface.gd
@@ -3,17 +3,16 @@ class_name PretreInterface extends CardInterface
 
 @onready var message: Message = $Message
 
-var enemy_selected:Player = null
 var enemies:Array[EnemyAI]
 
 func enter() -> void:
 	super() # Execute parenter enter func
-	enemy_selected = null
 	player_ref.need_to_select_enemy.emit(true) ## Enable enemy selection
 	enemies.assign(get_tree().get_nodes_in_group("Player").filter(func (enemy:Player) -> bool: return enemy != player_ref))
 	connect_enemies_listeners()
+	message.display_text("Qui?")
 	
-	#validate()
+	#
 #region Enemy selection
 func connect_enemies_listeners() -> void:
 	for enemy in enemies:
@@ -25,6 +24,8 @@ func disconnect_enemies_listeners() -> void:
 func select_enemy(enemy:Player) -> void:
 	disconnect_enemies_listeners()
 	player_ref.need_to_select_enemy.emit(false) ## Disable enemy selection
-	enemy_selected = enemy
-	print("Guard selected ",enemy_selected)
+	enemy.hand.cards[0].show_card()
+	await get_tree().create_timer(3).timeout
+	enemy.hand.cards[0].hide_card()
+	validate()
 #endregion
